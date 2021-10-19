b74c9324940c4e677bc670c47121b0a6aa80b8af
Nick <nphuynh11@gmail.com>
2021-10-18 21:03:29 -0400

    adding fade in animation to landing screen

---
+++ js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const animated = entry.target.querySelectorAll('.animated');
+      console.log(animated);
+      if (entry.isIntersecting) {
+        for (let i = 0; i < animated.length; i++) {
+          animated[i].classList.add('fadeInUp');
+        }
+      return; // if we added the class, exit the function
+      }
+
+      // We're not intersecting, so remove the class!
+      for (let i = 0; i < animated.length; i++) {
+        animated[i].classList.remove('fadeInUp');
+      }
+    });
+  });
+
+  observer.observe(document.querySelector('.one'));
+
+
+
+
+
+})
